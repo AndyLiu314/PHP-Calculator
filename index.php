5309f4e8c57fcc417249f1ba236c2757c7a4ac52
Andy Liu <andyliu4002@gmail.com>
2024-01-15 14:16:15 -0800

    finished basic calculator implementation

--- index.php
+++ index.php
@@ -24,9 +24,46 @@
 
         <?php 
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            // GRAB INPUT DATA
             $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
             $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
             $operator = htmlspecialchars($_POST["Operator"]);
+
+            // ERROR HANDLER
+            $errors = false;
+            if (empty($num01) || empty($num02) || empty($operator)) {
+                echo "<p class='calc-error'>Fill in all fields! </p>";
+                $errors = true;
+            } 
+
+            if (!is_numeric($num01) || !is_numeric($num02)) {
+                echo "<p class='calc-error'>Only write numbers! </p>";
+                $errors = true;
+            } 
+
+            // CALC NUMBERS IF NO ERRORS
+            if ($errors === false){
+                $value = 0;
+
+                switch ($operator){
+                    case "add":
+                        $value = $num01 + $num02;
+                        break;
+                    case "subtract":
+                        $value = $num01 - $num02;
+                        break;
+                    case "divide":
+                        $value = $num01 / $num02;
+                        break;
+                    case "multiply":
+                        $value = $num01 * $num02;
+                        break;
+                    default:
+                        echo "<p class='calc-error'>Calculator fucked up somehow </p>";
+                }
+
+                echo "<p class='calc-result'>Result = " . $value . "</p>";
+            }
         }
         ?>
     </main>
